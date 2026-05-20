3729cc02082a3b82182dc47bd1321ff5642eab8a
Ali Basel <202203874@Stu.uob.edu.bh>
2026-05-20 09:16:59 +0300

    dd load_reference_cache function to fetch categories, areas, and ingredients from API and save JSON

--- server.py
+++ server.py
@@ -10,4 +10,35 @@ if not os.path.exists(DATA_FOLDER):
     print(f"{DATA_FOLDER} folder created")
 else:
     print(f"{DATA_FOLDER} folder already exists")
-    
+
+def load_reference_cache():
+    cache = {}
+
+    API_CATEGORIES = "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
+    API_AREAS = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+    API_INGREDIENTS = "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
+
+    # Categories
+    r = requests.get(API_CATEGORIES)
+    cache['categories'] = [item['strCategory'] for item in r.json()['meals']]
+
+    # Areas
+    r = requests.get(API_AREAS)
+    cache['areas'] = [item['strArea'] for item in r.json()['meals']]
+
+    # Ingredients
+    r = requests.get(API_INGREDIENTS)
+    cache['ingredients'] = [item['strIngredient'] for item in r.json()['meals']]
+
+    # حفظ JSON
+    with open("data/reference_GroupX.json", "w") as f:
+        json.dump({
+            "categories": cache['categories'],
+            "areas": cache['areas'],
+            "ingredients": cache['ingredients'][:50]  
+        }, f, indent=4)
+
+    print("Reference cache loaded and saved in data/reference_GroupX.json")
+    return cache
+
+reference_cache = load_reference_cache()
